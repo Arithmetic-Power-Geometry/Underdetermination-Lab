78c4b4209d37ea04710c685a6d5ee3dd3bcf56b1
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-15 18:51:29 +0530

    Add deterministic manuscript compile script

---
+++ manuscript/compile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+xelatex -interaction=nonstopmode -halt-on-error main.tex
+biber main
+xelatex -interaction=nonstopmode -halt-on-error main.tex
+xelatex -interaction=nonstopmode -halt-on-error main.tex
